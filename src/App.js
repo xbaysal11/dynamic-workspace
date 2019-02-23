64ca58773e10cdce68a990edeb1575a2d5ad7c4d
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-02-23 13:49:04 +0600

    MODIFIED text

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
           <Clock format={"HH:mm:ss"} ticking={true} timezone={"KR/Pacific"} />
         </div>
         <div className="text">
+          <h1>FULLSTACK JS DEV</h1>
           <h1>DON'T WASTE YOUR TIME </h1>
-          <h1>STUDY MORE THAN OTHERS</h1>
           <h1>STUDY MORE THAN OTHERS</h1>
         </div>
       </div>
