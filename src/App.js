bb7459c85b6cd2d2c257e74ca627aed17d6f5031
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-02-23 10:35:22 +0600

    ADDED clock

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import Clock from "react-live-clock";
 import "./App.scss";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <div className="clock">
+          <Clock format={"HH:mm:ss"} ticking={true} timezone={"KR/Pacific"} />
+        </div>
+      </div>
+    );
   }
 }
 
